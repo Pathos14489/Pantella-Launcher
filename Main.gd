406ee8e0b7534d6b4a82d1220f90942eef842add
Pathos <administrator@foalcon.com>
2024-02-27 12:02:09 -0800

    first working version with build settings

--- Main.gd
+++ Main.gd
@@ -1,7 +1,20 @@
 extends Node
-@onready var PythonInterpretter = $".."
-@export var script_path = ""
-@export var open_console = true
-@export var watch = false
-# func _on_python_interpreter_interpreter_ready():
-# 	PythonInterpretter.run_script(script_path, open_console, watch)
+var DIR = OS.get_executable_path().get_base_dir() + "/"
+
+var dir = "res://repositories/"
+var temp_dir = "res://temp/"
+
+func _ready():
+	dir = "res://repositories/"
+	if !OS.has_feature("standalone"):
+		dir = ProjectSettings.globalize_path(dir)
+	else:
+		dir = DIR + dir
+	temp_dir = "res://temp/"
+	if !OS.has_feature("standalone"):
+		temp_dir = ProjectSettings.globalize_path(temp_dir)
+	else:
+		temp_dir = DIR + temp_dir
+    # Make sure the directory exists
+	DirAccess.make_dir_absolute(dir)
+	DirAccess.make_dir_absolute(temp_dir)
