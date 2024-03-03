f40bdb18470801ab9c9e1d4d08a5295ada5d5289
Pathos <administrator@foalcon.com>
2024-03-03 00:20:06 -0800

    added ease of use button support to the plugin buttons

--- plugin_button.gd
+++ plugin_button.gd
@@ -1,6 +1,8 @@
 extends Button
 
 @onready var root = get_tree().root.get_child(0)
+@onready var start_last_button = get_tree().root.get_child(0).get_node("UI/ScrollContainer2/VBoxContainer/SettingsPanel/VBoxContainer/StartLastButton")
+@onready var undeploy_button = get_tree().root.get_child(0).get_node("UI/ScrollContainer2/VBoxContainer/SettingsPanel/VBoxContainer/UndeployButton")
 
 var plugin = {
 	"name":"",
@@ -11,12 +13,23 @@ var plugin = {
 var repo = null
 
 func _on_pressed():
-	if text == plugin["name"]:
+	print("PluginButton: " + plugin["name"])
+	start_last_button.plugin = plugin
+	start_last_button.repository = repo
+	undeploy_button.plugin = plugin
+	undeploy_button.repository = repo
+	if text != "Stop":
 		text = "Stop"
+		start_last_button.text = "Stop"
+		undeploy_button.disabled = true
 	else:
 		text = plugin["name"]
+		start_last_button.text = "Start " + repo.repo["name"]
+		undeploy_button.disabled = false
 	if repo != null:
 		repo.plugin_selected(plugin)
+		start_last_button.visible = true
+		undeploy_button.visible = true
 
 func _ready():
 	text = plugin["name"]
