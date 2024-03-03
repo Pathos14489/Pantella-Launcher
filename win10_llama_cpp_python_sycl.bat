53e50693b940c06951964d04bf40a8bc3113d811
Pathos <administrator@foalcon.com>
2024-03-02 18:03:12 -0800

    somehow got these swapped. fixed it.

--- win10_llama_cpp_python_sycl.bat
+++ win10_llama_cpp_python_sycl.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set CMAKE_ARGS=-DLLAMA_VULKAN=on
+set CMAKE_ARGS=-DLLAMA_SYCL=on -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx
 set FORCE_CMAKE=1
 echo Updating scikit-build-core
 "%~dp0\python-3.10.11-embed\python.exe" -m pip install scikit-build-core --upgrade
