2538e2ba13424a88b8b51cbf203ac172a111adf2
Pathos <administrator@foalcon.com>
2024-03-03 00:20:43 -0800

    cleaned up win10 llama cpu script

--- win10_llama_cpp_python_cpu.bat
+++ win10_llama_cpp_python_cpu.bat
@@ -3,11 +3,7 @@ setlocal
 set CMAKE_ARGS=""
 set FORCE_CMAKE=0
 echo Updating scikit-build-core
-"%~dp0PythonFiles\python-3.10.11-embed\python.exe" -m pip install scikit-build-core --upgrade
+"%~dp0\python-3.10.11-embed\python.exe" -m pip install scikit-build-core --upgrade
 echo Updating llama-cpp-python
-"%~dp0PythonFiles\python-3.10.11-embed\python.exe" -m pip install llama-cpp-python --no-deps --no-cache-dir --force-reinstall --upgrade
-echo Cleaning up directory
-rmdir /s /q "./PythonFiles/llama_cpp_versions/cpu/ggufv2/%WIN_VERSION%/bin"
-rmdir /s /q "./PythonFiles/llama_cpp_versions/cpu/ggufv2/%WIN_VERSION%/lib"
-rmdir /s /q "./PythonFiles/llama_cpp_versions/cpu/ggufv2/%WIN_VERSION%/include"
+"%~dp0\python-3.10.11-embed\python.exe" -m pip install llama-cpp-python --no-deps --no-cache-dir --force-reinstall --upgrade
 echo Done
