f9e74c964d2d70d5ef4c8d3f9e8bdb6efd48585f
Pathos <administrator@foalcon.com>
2024-03-03 00:18:57 -0800

    added button to undeploy plugins

---
+++ UndeployButton.gd
@@ -0,0 +1,22 @@
+extends Button
+
+@onready var root = get_tree().root.get_child(0)
+
+var plugin = {
+	"name":"",
+	"repo":"",
+	"plugin_path":""
+}
+var repository = null
+
+func _on_root_settings_loaded():
+	visible = false
+	if "last_loaded_plugin" in root.settings and root.settings["last_loaded_plugin"]["name"] != "":
+		plugin = root.settings["last_loaded_plugin"]
+	if plugin != null:
+		visible = true
+	else:
+		visible = false
+
+func _on_pressed():
+	visible = false
